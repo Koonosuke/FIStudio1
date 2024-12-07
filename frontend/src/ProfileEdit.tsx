c1f1f69bfd84fe4336f5d35feb8a249e6b375d50
Haruta-Yamanaka <166776002+Haruta-Yamanaka@users.noreply.github.com>
2024-12-07 23:01:29 +0900

    profileを編集できるようになったぜ。

--- frontend/src/ProfileEdit.tsx
+++ frontend/src/ProfileEdit.tsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "./components/Header";
 import "./Profile.css";
+import "./ProfileEdit.css";
 
 function ProfileEdit(){
 //const [email, setEmail] = useState("");
 const [username, setUsername] = useState("");
+const [grade, setGrade] = useState("");
+const [pr, setPr] = useState("");
+
 const navigate = useNavigate();
 
 const body = new URLSearchParams();
 body.append("username", username);
+body.append("grade", grade);
+body.append("pr", pr);
 
 const HandleProfileClic =() =>{
     navigate("/profile");
@@ -59,13 +65,35 @@ const handleEdit = async () => {
         <div id="profile-screen">
             <div id="main-contents">
                 <h1>Profile</h1>
+                <h2>Username <br/>
                 <input
                     type="text"
                     placeholder="Usename"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     />
-                    <button onClick={handleEdit}>Edit</button>
+                </h2>
+                
+                <h2>Grade <br/>
+                <input
+                    type="text"
+                    placeholder="Grade"
+                    value={grade}
+                    onChange={(e) => setGrade(e.target.value)}
+                    />
+                    </h2>
+                <h2>PR <br/>
+                <textarea 
+                    value={pr}
+                    placeholder="Your PR"
+                    onChange={(e) => setPr(e.target.value)}
+                    cols={50} rows={10}>
+
+                    </textarea>
+                </h2>
+                
+                <button onClick={handleEdit}>Edit</button>
+                
             </div>
 
             <div id="buttons">
