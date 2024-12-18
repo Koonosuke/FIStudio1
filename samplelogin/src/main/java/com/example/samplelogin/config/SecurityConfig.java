be945ddbd3052d798a5e4269cf742eceb2d3d746
riporipo <22fi124@ms.dendai.ac.jp>
2024-12-18 15:59:10 +0900

    ラズパイ用設定

--- samplelogin/src/main/java/com/example/samplelogin/config/SecurityConfig.java
+++ samplelogin/src/main/java/com/example/samplelogin/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig implements WebSocketMessageBrokerConfigurer {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost"));
+        configuration.setAllowedOrigins(List.of("http://192.168.0.30"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
