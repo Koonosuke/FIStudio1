52c1d75694059c570e8069ff7d725c61e396953e
riporipo <22fi124@ms.dendai.ac.jp>
2024-12-23 00:32:11 +0900

    localhost_docker起動設定変更

---
+++ init/2_data.sql
@@ -0,0 +1,8 @@
+insert into users(email,username,password,isadmin,grade,pr) VALUES(
+    '22fi124@ms.dendai.ac.jp',
+    'riponeru',
+    '$2a$10$UekiPoN7FbEG13RxU7okae9c924EhOwEeygbCfO.7eexFxLuIY4mi',
+    true,
+    3,
+    '管理者でっす！'
+);
